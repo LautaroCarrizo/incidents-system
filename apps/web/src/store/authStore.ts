984cc0a8bf51a728abd8a42577447eb0e9d8f2d1
lautaro <laucarrizo332@gmail.com>
2025-12-14 21:58:35 -0300

    feat(web): add localStorage persistence to authStore

--- apps/web/src/store/authStore.ts
+++ apps/web/src/store/authStore.ts
@@ -1,7 +1,9 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-interface User {
-  id: string;
+export interface User {
+  id: number;
+  name: string;
   email: string;
   isAdmin: boolean;
 }
@@ -9,20 +11,27 @@ interface User {
 interface AuthState {
   user: User | null;
   token: string | null;
-  setAuth: (user: User, token: string) => void;
+  setAuth: (data: { user: User; token: string }) => void;
   clearAuth: () => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  token: null,
-  setAuth: (user: User, token: string) => {
-    console.log('setAuth called', { user, token });
-    set({ user, token });
-  },
-  clearAuth: () => {
-    console.log('clearAuth called');
-    set({ user: null, token: null });
-  },
-}));
+const STORAGE_KEY = 'auth-storage';
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      setAuth: (data: { user: User; token: string }) => {
+        set({ user: data.user, token: data.token });
+      },
+      clearAuth: () => {
+        set({ user: null, token: null });
+      },
+    }),
+    {
+      name: STORAGE_KEY,
+    }
+  )
+);
 
